890f8fd270f33c9ddfedd098a81e9ecfdf51f2c6
soeyusuke <s1240231@gmail.com>
2018-04-21 16:36:30 +0900

    [ADD] File con

--- main.go
+++ main.go
@@ -1,25 +1,67 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"log"
+	"os"
 
 	gc "github.com/rthornton128/goncurses"
 )
 
+type FileConfig struct {
+	file     *os.File
+	contents []string
+}
+
+func Openfile(filename string) (*FileConfig, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var str []string
+	for scanner.Scan() {
+		str = append(str, scanner.Text()+"\n")
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, errors.New(fmt.Sprintf("scanner err:", err))
+	}
+
+	return &FileConfig{
+		file:     file,
+		contents: str,
+	}, nil
+}
+
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: command <filename>\n")
+		os.Exit(1)
+	}
+
 	stdscr, err := gc.Init()
 	if err != nil {
 		log.Fatal("init", err)
 	}
-	defer gc.End()
+	defer gc.End() //endwin
 
-	stdscr.Refresh()
+	fc, err := Openfile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, v := range fc.contents {
+		fmt.Print(v)
+	}
+
 	for {
 		ch := stdscr.GetChar()
 		if ch == 'q' {
 			break
 		}
 	}
-	fmt.Println("good night")
 }
