606b2324d6dab5f33f5d79c5c3f40eb119f50b0d
soeyusuke <s1240231@gmail.com>
2018-04-30 01:51:26 +0900

    [ADD] go-homedir

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"log"
 	"os"
 
+	homedir "github.com/mitchellh/go-homedir"
 	gc "github.com/rthornton128/goncurses"
 )
 
-type FileConfig struct {
+type FileInfo struct {
+	namepath string
 	file     *os.File
 	contents []string
 }
@@ -46,11 +48,16 @@ func (m Mode) String() string {
 type View struct {
 	cursor Cursor
 	mode   Mode
-	window *gc.Window
+	window []*gc.Window
 }
 
-func OpenFile(filename string) (*FileConfig, error) {
-	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0755)
+func OpenFile(filename string) (*FileInfo, error) {
+	name, err := homedir.Expand(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		return nil, err
 	}
@@ -65,12 +72,18 @@ func OpenFile(filename string) (*FileConfig, error) {
 		return nil, errors.New(fmt.Sprintf("scanner err:", err))
 	}
 
-	return &FileConfig{
+	return &FileInfo{
+		namepath: name,
 		file:     file,
 		contents: str,
 	}, nil
 }
 
+func (f *FileInfo) GetLine() int {
+	return len(fc.contents)
+}
+
+// windowの設定、ファイルの表示をする
 func (v *View) Init(contents []string) error {
 	gc.Raw(true) // raw mode
 	gc.Echo(false)
@@ -91,10 +104,6 @@ func (v *View) Init(contents []string) error {
 		v.window.Print(contents[i])
 		v.window.Refresh()
 	}
-	//	for _, val := range contents {
-	//		v.window.Print(val)
-	//		v.window.Refresh()
-	//	}
 	v.window.Move(0, 0) // init locate of cursor
 	v.window.Resize(line, x)
 	v.window.Refresh()
@@ -102,6 +111,7 @@ func (v *View) Init(contents []string) error {
 	return nil
 }
 
+// Normal mode時のキー操作
 func (v *View) NormalCommand(ch gc.Key) error {
 	switch ch {
 	case gc.KEY_LEFT, 'h':
@@ -116,7 +126,7 @@ func (v *View) NormalCommand(ch gc.Key) error {
 		if v.cursor.y > 0 {
 			v.cursor.y--
 		}
-	case gc.KEY_DOWN, 'j':
+	case gc.KEY_DOWN, 'j', '\n':
 		if v.cursor.y < v.cursor.max_y {
 			v.cursor.y++
 		}
@@ -129,7 +139,7 @@ func NewView(w *gc.Window) *View {
 	return &View{
 		cursor: Cursor{x: 0, y: 0},
 		mode:   Normal,
-		window: w,
+		window: w, //window の数によっては増やす(mapでもいい？)
 	}
 }
 
@@ -151,7 +161,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	view := NewView(stdscr)
+	view := NewView() //ここでサブウィンドウの作成
 	if err := view.Init(fc.contents); err != nil {
 		log.Fatal(err)
 	}
